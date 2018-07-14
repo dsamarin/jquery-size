7bcc2eda502b35a2bfc33efccd4d75d6a69e3917
Devin Samarin <me@dsamar.in>
2018-07-14 03:09:06 -0700

    csv: Write row for header

--- csv.go
+++ csv.go
@@ -8,6 +8,16 @@ import (
 func outputCSV(writer io.Writer, stats []*SizeInfo) error {
 	cw := csv.NewWriter(writer)
 
+	cw.Write([]string{
+		"Version",
+		"Regular",
+		"Gzipped",
+		"Zopflinated",
+		"Minified",
+		"Minified+Gzipped",
+		"Minified+Zopflinated",
+	})
+
 	for _, stat := range stats {
 		err := cw.Write(stat.CSVRecord())
 		if err != nil {
